6b0401e61a65e2cf1aad57485e940e6281bc5980
tinysaint <stpeng520123@163.com>
2020-06-03 03:13:55 +0000

    admin can detele users

--- weibo/app/Http/Controllers/UsersController.php
+++ weibo/app/Http/Controllers/UsersController.php
@@ -68,6 +68,14 @@ class UsersController extends Controller
         return redirect()->route('users.show', $user);
     }
 
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
+    }
+
     public function __construct()
     {
         $this->middleware('auth', [
